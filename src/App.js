622691919489177f2852bd74885a72d9ecc16f94
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-08 17:44:10 -0300

    :construction: chore: project folder arranged
    unnecessary stuff removed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Routes from "./routes";
+// styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import Gaps from "./styles/settings/gaps";
+import Elements from "./styles/base/elements";
+import Reset from "./styles/generic/reset";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Gaps />
+      <Elements />
+      <Reset />
+
+      <Routes />
+    </>
   );
-}
+};
 
 export default App;
