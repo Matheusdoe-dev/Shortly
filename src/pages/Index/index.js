88248814602b0043dbd93918a988dbea61218907
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-08 17:45:54 -0300

    :lipstick: feat: Index page

---
+++ src/pages/Index/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+// components
+import Header from "../../components/Header";
+import Hero from "../../components/Hero";
+import ShorterForm from "../../components/Shorter-Form";
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <ShorterForm />
+    </>
+  );
+};
+
+export default Index;
