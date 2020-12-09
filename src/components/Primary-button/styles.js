2f0088d3d0594273b2be04d663cc9c34ba2b52f8
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-09 17:49:19 -0300

    :lipstick: chore: font weight 700 applied to Primary Button text

--- src/components/Primary-button/styles.js
+++ src/components/Primary-button/styles.js
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 // styles
 import colors from "../../styles/settings/colors";
+import borders from "../../styles/tools/borders";
 // components
 import { Button } from "react-bootstrap";
 
 export const PrimaryButtonWrapper = styled(Button)`
   & {
     padding: var(--gap-sm) var(--gap-lg);
-    border-radius: 50px;
+    border-radius: ${borders.radius.buttons};
     background: ${colors.primary.cyan};
     border: none;
+
+    font-weight: 700;
   }
 `;
