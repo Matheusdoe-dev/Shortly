469c2eab413887ef0a07acc00d78eae3ba102703
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-09 17:50:22 -0300

    :lipstick: chore: Benefits and Boost sections added to Index page

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import ShorterForm from "../../components/Shorter-Form";
+import BenefitsSection from "../../components/Benefits-section";
+import BoostSectionCTA from "../../components/Boost-section-cta";
 
 const Index = () => {
   return (
@@ -10,6 +12,8 @@ const Index = () => {
       <Header />
       <Hero />
       <ShorterForm />
+      <BenefitsSection />
+      <BoostSectionCTA />
     </>
   );
 };
