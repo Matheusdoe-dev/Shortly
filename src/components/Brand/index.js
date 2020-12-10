db12a29c88f36461386314f1f4b58deb5ba78847
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-10 17:52:38 -0300

    :lipstick: feat: Brand component created

---
+++ src/components/Brand/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+// imgs
+import logo1Img from "../../assets/images/logo.svg";
+import logo2Img from "../../assets/images/logo-2.svg";
+// styles
+import { BrandWrapper } from "./styles";
+
+const logos = {
+  primary: logo1Img,
+  secondary: logo2Img,
+};
+
+const Brand = ({ secondary }) => {
+  return (
+    <BrandWrapper href="/">
+      {secondary ? (
+        <img src={logos.secondary} alt="Shortly" />
+      ) : (
+        <img src={logos.primary} alt="Shortly" />
+      )}
+    </BrandWrapper>
+  );
+};
+
+export default Brand;
---
+++ src/components/Brand/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const BrandWrapper = styled.a`
+  & {
+    display: block;
+    box-sizing: initial;
+
+    width: 130px;
+    height: 36px;
+
+    margin: 0 auto;
+
+    img {
+      width: 130px;
+      height: 36px;
+
+      margin: 0 auto;
+    }
+  }
+`;
