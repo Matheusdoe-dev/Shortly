397d692597e784f8ed1c9898c18db0bd4aa05520
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-11 15:31:47 -0300

    :iphone: chore: handle Hero component to large devices

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,32 +1,28 @@
 import React from "react";
 // imgs
 import heroImg from "../../assets/images/illustration-working.svg";
-// components
-import { Container, Row, Col } from "react-bootstrap";
 // styles
-import { HeroWrapper, HeroImg } from "./styles";
+import { HeroWrapper, HeroImg, HeroContent, HeroContainer } from "./styles";
 import PrimaryButton from "../Primary-button";
 
 const Hero = () => {
   return (
     <HeroWrapper>
-      <Container fluid>
-        <Row>
-          <Col xs="12">
-            <HeroImg>
-              <img src={heroImg} alt="Working illustration" />
-            </HeroImg>
-          </Col>
-          <Col xs="12">
-            <h1>More than just shorter links</h1>
-            <p>
-              Build your brand's recognition and get detailed insights on how
-              your links are perfoming.
-            </p>
-            <PrimaryButton>Get Started</PrimaryButton>
-          </Col>
-        </Row>
-      </Container>
+      <HeroContainer>
+        <HeroImg>
+          <img src={heroImg} alt="Working illustration" />
+        </HeroImg>
+
+        <HeroContent>
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand's recognition and get detailed insights on how your
+            links are perfoming.
+          </p>
+
+          <PrimaryButton>Get Started</PrimaryButton>
+        </HeroContent>
+      </HeroContainer>
     </HeroWrapper>
   );
 };
--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,26 +1,28 @@
 import styled from "styled-components";
 // styles
 import colors from "../../styles/settings/colors";
+import grid from "../../styles/objects/grid";
 
 export const HeroWrapper = styled.section`
   & {
-    padding: var(--gap-sm) 0 var(--gap-xl) 0;
+    background: white;
+  }
+`;
 
-    h1 {
-      text-align: center;
-      margin-top: var(--gap-md);
-    }
+export const HeroContainer = styled.div`
+  & {
+    ${grid.container}
 
-    p {
-      text-align: center;
-      color: ${colors.neutral.grayish_violet};
-      margin: var(--gap-md) auto 0 auto;
-    }
+    padding: var(--gap-sm) 15px calc(var(--gap-xl) * 2) 15px;
 
-    button {
-      text-align: center;
-      display: block;
-      margin: var(--gap-md) auto 0 auto;
+    display: grid;
+    grid-template-columns: 1fr;
+
+    @media (min-width: 768px) {
+      grid-template-columns: 7fr 5fr;
+      grid-template-areas: "content image";
+      align-items: center;
+      padding-top: var(--gap-xl);
     }
   }
 `;
@@ -32,6 +34,45 @@ export const HeroImg = styled.div`
 
     img {
       width: 150%;
+    }
+
+    @media (min-width: 768px) {
+      grid-area: image;
+    }
+  }
+`;
+
+export const HeroContent = styled.article`
+  & {
+    h1 {
+      text-align: center;
+      margin-top: var(--gap-md);
+    }
+
+    p {
+      text-align: center;
+      color: ${colors.neutral.grayish_violet};
+    }
+
+    button {
+      text-align: center;
+      display: block;
+      margin: var(--gap-md) auto 0 auto;
+    }
+
+    @media (min-width: 768px) {
+      grid-area: content;
+      column-gap: var(--gap-md);
+
+      h1,
+      p {
+        text-align: left;
+      }
+
+      button,
+      p {
+        margin: var(--gap-md) 0 0 0;
+      }
     }
   }
 `;
