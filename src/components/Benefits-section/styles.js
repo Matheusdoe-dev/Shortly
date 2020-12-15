b66e0d145971eee2cafc5b08e04a070a819ddfd1
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-15 15:03:28 -0300

    :ambulance: chore: benefits icon position responsiveness fixed

--- src/components/Benefits-section/styles.js
+++ src/components/Benefits-section/styles.js
@@ -72,8 +72,24 @@ export const BenefitsIcon = styled.div`
       height: 36px;
     }
 
-    @media (min-width: 768px) {
+    @media (max-width: 575.98px) {
+      left: 41%;
+    }
+
+    @media (min-width: 576px) and (max-width: 767.98px) {
       left: 33%;
+    }
+
+    @media (min-width: 768px) and (max-width: 991.98px) {
+      left: 35%;
+    }
+
+    @media (min-width: 992px) and (max-width: 1199.98px) {
+      left: 37%;
+    }
+
+    @media (min-width: 1200px) {
+      left: 40%;
     }
   }
 `;
