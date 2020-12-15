51af9be28c3849aa40b74ab8f5fda3a0257d4ede
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-12-15 15:02:34 -0300

    :sparkles: feat: shorter functionality implemented

--- src/hooks/shorter-hook.js
+++ src/hooks/shorter-hook.js
@@ -4,24 +4,53 @@ import shrtcodeApi from "../services/shrtcodeApi";
 
 // https://app.shrtco.de/docs
 
-export const useShorter = () => {
+const useShorter = () => {
   const [url, setURL] = useState("");
 
   const [error, setError] = useState("");
 
+  const [loadingStatus, setLoadingStatus] = useState(false);
+
   const handleSubmitURL = async (event) => {
     event.preventDefault();
+    setLoadingStatus(true);
 
     if (!url) {
       setError("Please add a link");
+      setLoadingStatus(false);
       return;
     }
 
-    await shrtcodeApi.post(`${url}`).catch((err) => {
-      setError("Unable to short url, try again");
-      alert(err);
-    });
+    await shrtcodeApi({
+      method: "GET",
+      url: "/shorten",
+      params: {
+        url,
+      },
+    })
+      .then((r) => {
+        if (localStorage.getItem("urls")) {
+          const old_data = JSON.parse(localStorage.getItem("urls"));
+
+          old_data.push({ url, short_link: r.data.result.short_link });
+
+          localStorage.setItem("urls", JSON.stringify(old_data));
+        } else {
+          const new_data = [{ url, short_link: r.data.result.short_link }];
+
+          localStorage.setItem("urls", JSON.stringify(new_data));
+        }
+
+        alert("URL shorted succesful");
+      })
+      .catch((err) => {
+        setError(err);
+        alert(`Unable to short url, ${err}`);
+      })
+      .finally(() => setLoadingStatus(false));
   };
 
-  return { handleSubmitURL, url, setURL };
+  return { handleSubmitURL, url, setURL, error, loadingStatus };
 };
+
+export default useShorter;
